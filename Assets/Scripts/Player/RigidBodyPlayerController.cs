acae52bfd2f99e1a4e22fd66042cc680220c56be
Andrew Rae <arae275@gmail.com>
2017-12-18 16:16:06 +0000

    Set movement type enum added. Set switch for movement type, reduced player speed to zero if movement magnitude is less that 1, previously im not sure if it ever would have hit zero... Added doco.

--- Assets/Scripts/Player/RigidBodyPlayerController.cs
+++ Assets/Scripts/Player/RigidBodyPlayerController.cs
@@ -11,12 +11,20 @@ namespace Hephaestus.Player.Controller{
     [RequireComponent(typeof(Rigidbody))]
     public class RigidBodyPlayerController : MonoBehaviour
     {
+        /// <summary>
+        /// Player choice of first or third person camera
+        /// </summary>
         public PlayerCameraEnum CameraChoice;
+
+        /// <summary>
+        /// Represents if lerped or raw movement
+        /// </summary>
+        public MovementTypeEnum MovementType;
 
         #region Private Fields
         private PersonalCameraController cameraController;
         private Rigidbody rb;
-        private Vector3 targetVector;
+        private Vector3 targetVector;      
         #endregion
 
         /// <summary>
@@ -32,23 +40,53 @@ namespace Hephaestus.Player.Controller{
         /// </summary>
         public void FixedUpdate()
         {
+            ///Get input from player
             GetInput();
+
             ///Set target position by adding targetVector to current vector
             Vector3 targetPosition = new Vector3(this.gameObject.transform.position.x + targetVector.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z + targetVector.z);
 
             ///Smooth between current position and target position
-            gameObject.transform.position = Vector3.Lerp(transform.position, targetPosition, 0.1f);
+            switch (MovementType)
+            {
+                ///Directly move transform to position
+                case MovementTypeEnum.Raw:
+                    transform.position = targetPosition;
+                    break;
+                ///Lerp into position
+                case MovementTypeEnum.Smoothed:
+                    gameObject.transform.position = Vector3.Lerp(transform.position, targetPosition, 0.1f);
+                    break;
+            }           
 
-            ///Decelerate player
-            targetVector = new Vector3(targetVector.x / 2, targetVector.y, targetVector.z / 2);
+            ///If the players movement magnitude is less than 1, but not 0, set it to zero for optimisation.
+            if (targetVector.magnitude < 1 && targetVector.magnitude != 0)
+            {
+                targetVector.x = 0;
+                targetVector.y = 0;
+                targetVector.z = 0;
+            }
+            ///If it hasn't been set to zero, decelerate.
+            else if (targetVector.magnitude != 0)
+            {
+                ///Decelerate player
+                targetVector = new Vector3(targetVector.x / 2, targetVector.y, targetVector.z / 2);
+            }
         }
 
+        /// <summary>
+        /// Takes input on Horizontal and Verical (keyboard)
+        /// Takes input on X and Y (mouse)
+        /// Positions camera and sets target position based on these inputs.
+        /// </summary>
         private void GetInput()
         {
+            ///Keyboard Input
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
             targetVector = new Vector3(targetVector.x + horizontal, 0, targetVector.z + vertical);
 
+            ///Mouse Input
             float mouseX = Input.GetAxis("Mouse X");
             float mouseY = Input.GetAxis("Mouse Y");
             gameObject.transform.Rotate(0, mouseX, 0);
